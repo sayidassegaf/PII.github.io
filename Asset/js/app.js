7af297f51e9ad81aff8bd99b590ede25b10967a1
sayidassegaf <sayid.assegaf12@gmail.com>
2023-04-28 06:41:23 +0800

    first commit

---
+++ Asset/js/app.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+
+
+    new Swiper('.slides', {
+        speed: 600,
+        loop: true,
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false
+        },
+        slidesPerView: 'auto',
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 20
+            },
+
+            1200: {
+                slidesPerView: 3,
+                spaceBetween: 1
+            }
+        }
+    });
+
+
+
+    //HERO SLIDER
+});
